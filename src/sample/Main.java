6c21a1ed58e0210c2bf05ab302637c21f98177ba
p-od <pascal.odermatt@bedag.ch>
2017-11-06 12:55:06 +0100

    SetResizable = false, fixed height and width

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Listbuilder");
+        primaryStage.setScene(new Scene(root, 600, 800));
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
