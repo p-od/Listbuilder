fd9bd99f29b51b706573f3644bb0ceeb8302dcae
Gammi <fgammi@gmail.com>
2017-11-20 13:14:29 +0100

    Tests erfasst

---
+++ Test/Listbuilder/TestListe.java
@@ -0,0 +1,19 @@
+package Listbuilder;
+
+import Listbuilder.Model.Liste;
+
+public class TestListe {
+
+    @TEST
+    public void createNewListeTest() {
+        Liste liste = createNewListe("Test");
+        assertTrue(liste.getListeName().equals("Test"));
+    }
+
+    @TEST
+    public void deleteListeTest() {
+        Liste liste = createNewListe("Test");
+        liste.delete("Test");
+        assertFalse(liste.hasElement("Test"));
+    }
+}
