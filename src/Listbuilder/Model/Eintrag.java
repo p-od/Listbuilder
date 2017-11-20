66228d932161597ed8b808b83290e55d4a67271a
Kevin Kohler <kevin.kohler94@gmail.com>
2017-11-20 12:20:03 +0100

    modelklassen erweitert

--- src/Listbuilder/Model/Eintrag.java
+++ src/Listbuilder/Model/Eintrag.java
@@ -1,4 +1,38 @@
 package Listbuilder.Model;
 
 public class Eintrag {
+
+    private int id;
+
+    private boolean erledigt;
+
+    private String name;
+
+    public Eintrag() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isErledigt() {
+        return erledigt;
+    }
+
+    public void setErledigt(boolean erledigt) {
+        this.erledigt = erledigt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/Listbuilder/Model/Liste.java
+++ src/Listbuilder/Model/Liste.java
@@ -5,10 +5,46 @@ import java.util.List;
 public class Liste {
 
     private int id;
-    private List<Eintrag> eintraege;
+
     private boolean erledigt;
+
+    private String name;
+
+    private List<Eintrag> eintraege;
 
     public Liste() {
 
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isErledigt() {
+        return erledigt;
+    }
+
+    public void setErledigt(boolean erledigt) {
+        this.erledigt = erledigt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Eintrag> getEintraege() {
+        return eintraege;
+    }
+
+    public void setEintraege(List<Eintrag> eintraege) {
+        this.eintraege = eintraege;
     }
 }
